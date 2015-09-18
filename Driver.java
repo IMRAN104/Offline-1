52ec404a7976f1377500fe0b607c1a37ce561c87
IMRAN104 <1305104.se@ugrad.cse.buet.ac.bd>
2015-09-19 00:07:41 +0600

    First commit

---
+++ Driver.java
@@ -0,0 +1,43 @@
+package Offline1;
+
+/**
+ * Created by EMON on 18-Sep-15.
+ */
+public class Driver {
+    private String name;
+    private  int age;
+    private String country;
+    public  Driver() {
+
+    }
+    public  Driver(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    public  Driver(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public  String getName()  {
+        return name;
+    }
+
+    public  String getCountry()  {
+        return country;
+    }
+
+    public  int getAge()  {
+        return age;
+    }
+
+    public  void setCountry(String country) {
+        this.country = country;
+    }
+
+    public  void print()  {
+        //cout << name << " Age: " << age << " Country: " << country << endl;
+        System.out.println(name+" Age: "+age+" Country: "+country);
+    }
+}
